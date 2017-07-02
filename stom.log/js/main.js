b2b7f98a179689f8ff62ecf55aba9e5cb5c06e67
webmaster9110 <slavikweb9110@gmail.com>
2017-07-02 23:04:19 +0300

    initial commit

---
+++ stom.log/js/main.js
@@ -0,0 +1,29 @@
+
+  // slider
+  $(document).ready(function(){
+    $('.slide').slick({
+     autoplay: false,
+     slidesToShow: 3,
+     speed: 400,
+     dots:false,
+       responsive: [
+    {
+      breakpoint: 1000,
+      settings: {
+        slidesToShow: 1,
+      }
+    },
+    ]
+   });
+  });
+  
+// nav menu
+jQuery(document).ready(function($) {
+// hide the menu when the page load
+$("#navigation-list").hide();
+// when .menuBtn is clicked, do this
+$(".menuBtn").click(function() {
+// open the menu with slide effect
+$("#navigation-list").slideToggle(300);
+});
+});
